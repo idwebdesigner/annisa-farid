551e6a9cdebf710984f9512402737142db04807b
@faridiot <dev@designer.web.id>
2017-12-07 18:15:12 +0700

    remove unrelevant

--- contact.php
+++
@@ -1,3 +0,0 @@
-
-<!-- saved from url=(0056)http://www.enableds.com/products/appeca4/php/contact.php -->
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body><div class="line-gutter-backdrop"></div><table><tbody><tr><td class="line-number" value="1"></td><td class="line-content"><span class="html-end-of-file"></span></td></tr></tbody></table></body></html>
